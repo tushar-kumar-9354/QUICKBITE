ffbd706d27c8a33f4403544a729b2a17ab800726
kpregtech <kpregtech@gmail.com>
2026-01-28 19:47:46 +0530

    completed app

--- API LEARNING THROUGH PROJECT/QUICKBITE/wsgi.py
+++ API LEARNING THROUGH PROJECT/QUICKBITE/wsgi.py
@@ -1,10 +1,3 @@
-# wsgi.py
-import sys
-import os
-
-# Add the current directory to the Python path
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from app import create_app
 
 app = create_app()
